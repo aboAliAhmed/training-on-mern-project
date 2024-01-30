ea29f4f0ff0f2ed187a60ffb7772615a7672c1ee
aboAliAhmed <ahmedbeasar@gmail.com>
2024-01-30 20:26:09 +0200

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        About Sahand Estate
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur
+        asperiores cum architecto accusantium facere? Expedita facilis enim eum 
+        vitae architecto saepe consequuntur, quo, asperiores adipisci harum in 
+        consequatur modi itaque.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur
+        asperiores cum architecto accusantium facere? Expedita facilis enim eum 
+        vitae architecto saepe consequuntur, quo, asperiores adipisci harum in 
+        consequatur modi itaque.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur
+        asperiores cum architecto accusantium facere? Expedita facilis enim eum 
+        vitae architecto saepe consequuntur, quo, asperiores adipisci harum in 
+        consequatur modi itaque.
+      </p>
+    </div>
   )
 }
